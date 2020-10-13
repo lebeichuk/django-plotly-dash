f9c1f5d5639376e7e806e4d927f7bf6d246539f8
Oleksandr Lebeichuk <lebeichuk.sasha@gmail.com>
2020-10-13 21:06:32 +0300

    -implemented example of stripe subscription with django

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import Checkoutpage
 from home.dash_apps.finished_apps import simpleexample
 
+
 urlpatterns = [
-    path('', views.home, name='home')
+    # path('', views.home, name='home'),
+    path('checkout/', Checkoutpage.as_view(), name='checkout'),
 ]
